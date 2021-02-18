74fc0927f9448ad5f48d1596a7947746c0b2ec10
Jean Nunes <jaon@itriad.org.br>
2021-02-18 10:01:50 -0400

    Adicionando Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <CssBaseline />
       <Routes />
     </BrowserRouter>
   );
